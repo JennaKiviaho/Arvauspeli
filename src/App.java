dd02cce3b721d963399fa5d3ada47eb9c6b659cd
Jenna Kiviaho <jenna.kiviaho@hotmail.com>
2024-09-10 13:56:27 +0300

    Käyttäjänsyöte luotu

--- src/App.java
+++ src/App.java
@@ -1,5 +1,14 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        Scanner input = new Scanner(System.in);
+
+        String nimi = "Jenna"; // Oikea vastaus on Jenna
+
+        System.out.println("Arvaa nimeni.");
+        nimi = input.nextLine();
+        
     }
 }
