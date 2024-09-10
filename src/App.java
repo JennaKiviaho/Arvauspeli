d60b9f44d146daf681b7ee48c0cfc1e6b76905c5
Jenna Kiviaho <jenna.kiviaho@hotmail.com>
2024-09-10 17:25:51 +0300

    Toistorakennetta päivitetty
    Lasketaan arvaukset ja voi päättää pelin kirjoittamalla "loppu". Pelin päättyessä kerrotaan arvauksien määrä.

--- src/App.java
+++ src/App.java
@@ -6,21 +6,32 @@ public class App {
         Scanner input = new Scanner(System.in);
 
         String nimi = "Jenna"; // Oikea vastaus on Jenna
+        int arvaus = 0;
 
         System.out.println("Arvaa nimeni.");
         
 
-        do
+        do // Toistorakenne
         {
         nimi = input.next();
-        if (nimi.equals("Jenna"))
+
+        if (nimi.equals("Jenna")) // Jos vastaus on oikein
         {
             System.out.println("Oikein! Onneksi olkoon!");
+            arvaus++;
         }
-        else
+        if (!nimi.equals("Jenna")) // Jos vastaus on väärin
         {
-            System.out.println("Väärin. Arvaa uudelleen.");
+            System.out.println("Väärin. Arvaa uudelleen. Tai kirjoita loppu päättääksesi pelin.");
+            arvaus++;
         }
-    } while (!nimi.equals("Jenna"));
+        if (nimi.equalsIgnoreCase("loppu"))
+        {
+            arvaus--;
+            break;
+        }
+        } while (!nimi.equals("Jenna"));
+
+        System.out.println("Arvasit " + arvaus + " kertaa.");
     }
 }
